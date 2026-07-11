f6cd604443d9b16e9a1930bcc1c4b86a8a5016b7
lucasfeliciana <lfeliciana@byupathway.edu>
2026-07-11 14:10:24 -0500

    temple-album activity

---
+++ scripts/temples.js
@@ -0,0 +1,20 @@
+const year = document.querySelector("#currentyear");
+
+year.textContent = new Date().getFullYear();
+
+const lastModified = document.querySelector("#lastModified");
+
+lastModified.textContent = `Last Modification: ${document.lastModified}`;
+
+const menuButton = document.querySelector("#menu");
+const navigation = document.querySelector("nav");
+
+menuButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+
+    if (navigation.classList.contains("open")) {
+        menuButton.textContent = "✖";
+    } else {
+        menuButton.textContent = "☰";
+    }
+});
