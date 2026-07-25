bd74daf49d2d32d563cb7819ea2fca6f26af703e
lucasfeliciana <lfeliciana@byupathway.edu>
2026-07-24 20:47:33 -0500

    changes made

--- scripts/place.js
+++ scripts/place.js
@@ -8,19 +8,13 @@ const temperature = 40;
 const windSpeed = 8;
 
 function calculateWindChill(temp, speed) {
-    return (
-        35.74 +
-        (0.6215 * temp) -
-        (35.75 * Math.pow(speed, 0.16)) +
-        (0.4275 * temp * Math.pow(speed, 0.16))
-    );
+    return 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
 }
 
 const windChill = document.querySelector("#wind-chill");
 
 if (temperature <= 50 && windSpeed > 3) {
-    windChill.textContent =
-        `${calculateWindChill(temperature, windSpeed).toFixed(1)} °F`;
+    windChill.textContent = `${calculateWindChill(temperature, windSpeed).toFixed(1)} °F`;
 } else {
     windChill.textContent = "N/A";
 }
