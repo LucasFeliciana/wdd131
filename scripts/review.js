b00975f03f74b015e978e981a0d7695d22b4557e
lucasfeliciana <lfeliciana@byupathway.edu>
2026-08-01 12:27:11 -0500

    changes on js

--- scripts/review.js
+++ scripts/review.js
@@ -11,3 +11,5 @@ reviewCountDisplay.textContent = reviewCount;
 const currentYear = document.querySelector("#currentyear");
 const lastModified = document.querySelector("#lastModified");
 
+currentYear.textContent = new Date().getFullYear();
+lastModified.textContent = `Last Modification: ${document.lastModified}`;
